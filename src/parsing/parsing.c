2f22c838157fa133e7805e18e502cb27121d3bc6
cafabre <camille.fabre003@gmail.com>
2025-12-19 21:36:02 +0100

    created a complete tester with a tester.txt that references all the expected results for each .rt file

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: cafabre <cafabre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 13:23:45 by cafabre           #+#    #+#             */
-/*   Updated: 2025/12/19 18:00:10 by cafabre          ###   ########.fr       */
+/*   Updated: 2025/12/19 18:30:05 by cafabre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,37 +78,3 @@ int     parsing(int fd, t_scene *s, t_data *data)
     
     return (EXIT_SUCCESS);
 }
-
-/*
-RESTE A FAIRE
-- refactor of functions over 25 lines -> ONGOING
-    -> parse_ids DONE
-    -> dispatch_ids DONE
-    -> dispatch_scene TO DO
-    -> check_coords DONE
-    -> display_error_message DONE
-    -> ft_atof DONE
-    -> ft_isnumber DONE
-- move libft_utils.c functions into the libft
-
-ISSUES
-- ambient lighting isnt right (ex : amb max + no light -> shadows houldnt appear) -> exec
-- add the code to make create_cy(...) work -> render of cylinders not tested yet -> exec
-- pixelated results in some cases (recurring issue in minirt, solutions easily found on internet) -> exec
-    --> see room.rt
-
-FIXED
-- "invalid number of arguments on one line" when whitespaces at the end of the line -> parsing
-    --> fix : ft_strtrim(line, " \t\n") before split
-- "invalid number of arguments on one line" when wrong id at the beginning of the line -> parsing
-    --> fix : changed the main dispatcher logic
-- need to check that there is at least one C, one L and one A -> parsing
-    --> fix : added a check in parse_ids at the end of the loop
-
-IMPROVEMENTS
-- ft_strcmp to get the id -> called twice (dispatch_id and dispatch_scene / dispatch_obj) -> parsing
-    --> find a way to keep only one check
-
-BONUS
-- add color to light -> NOT FINISHED (errors to handle)
-*/
